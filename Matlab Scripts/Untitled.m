95610578568d393bd886cc8201bce8a0f7ba5bf2
Daniel Miller <Peaches491@gmail.com>
2014-04-25 15:58:33 -0400

    Method for point checking added

--- Matlab Scripts/Untitled.m
+++ Matlab Scripts/Untitled.m
@@ -1,4 +1,4 @@
-function x =  Untitled(a)
+function Untitled(a)
     d_x = a(1);
     d_y = a(2);
     d_z = a(3);
@@ -10,11 +10,5 @@ function x =  Untitled(a)
     y = y*r + d_y;
     z = z*r + d_z;
 
-    surf2patch(surf(x, y, z, 'FaceColor', 'y', 'FaceAlpha', 1.0, 'EdgeColor', 'none'));
-
-
-%     c = [d_x, d_y, d_z];
-%     X = [0, 0, 3];
-%     inside = sum((c-X).^2) <= r^2;
-    x = 0;
+    surf2patch(surf(x, y, z, 'FaceColor', 'y', 'FaceAlpha', 0.2, 'EdgeColor', 'none'));    
 end    
