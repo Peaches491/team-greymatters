055dfe2b9691e53fb0a8599c4642ffdfce6222f8
mjbrauck <mjbrauck@gmail.com>
2014-04-26 12:12:30 -0400

    Fixed Scaling bug and 0 Radius bug
    added globals for const paramaters of links width, depth, spacing

--- Matlab Scripts/RCM Modeling/RCMtest2.m
+++ Matlab Scripts/RCM Modeling/RCMtest2.m
@@ -1,27 +1,36 @@
 clc; close all; clear all; 
+ 
+global endLength  
+endLength = .0254; %the length of the end of a link determins spaceing for paralel links 
+global width
+width = .006; % the width of all links (should get from CADD Model)
+global depth, 
+depth = .006; % the z depth for all links (material thickness)
+global spacing 
+spacing = .02; % the space between the two sets of four-bars  
 
-L1 = 5;
-L2 = 5;
-r1 = .5;
-r2 = .5;
-L3 = 5;
+L1 = .1;
+L2 = .1;
+L3 = .1;
+r1 = 0;
+r2 = 0;
 
-p1 = [5,5,5]; %entry location 
-p2 = [0,5,0]; %workspace location 
-
-
-[theta1, theta2, Tx, Ty, Tz] = RCMInvKin(L1, L2, L3, p1, p2);
-
-
-
-% [L1X L1Y L1Z L2X L2Y L2Z] = makeRCM(L1, L2, L3, r1, r2, Tz, Ty, Tx, theta1, theta2);
+% p1 = [5,5,5]; %entry location 
+% p2 = [0,5,0]; %workspace location 
 % 
-% clf;
-% view(3);
-% PlotRCM(Tx,Ty,Tz,theta1,theta2,L3,L1,L2);
-% hold on;
-% plotModel(L1X, L1Y, L1Z, L2X, L2Y, L2Z);
+% 
+% [theta1, theta2, Tx, Ty, Tz] = RCMInvKin(L1, L2, L3, p1, p2);
+
+%makeLink(.1, .01, 30, 1);
+
+[L1X L1Y L1Z L2X L2Y L2Z] = makeRCM(L1, L2, L3, r1, r2, 0, 0, 0, 0, 0);
+
+clf;
+view(3);
+PlotRCM(0,0,0,0,0,L3,L1,L2);
+hold on;
+plotModel(L1X, L1Y, L1Z, L2X, L2Y, L2Z);
 
 
-% axis([-10, 10, -10, 10, -10, 10]);
-% axis equal
+axis([-.5, .5, -.5, .5, -.5, .5]);
+axis equal
--- Matlab Scripts/RCM Modeling/makeRCM.m
+++ Matlab Scripts/RCM Modeling/makeRCM.m
@@ -1,9 +1,12 @@
 function [L1X L1Y L1Z L2X L2Y L2Z] = makeRCM(L1, L2, L3, r1, r2, Tz, Ty, Tx, theta1, theta2)
 
+global endLength 
+global depth
+global spacing
 
 %% Link 1
-[L1S1X L1S1Y L1S1Z] = makeLink(L1,.5,30,1);
-[L1S2X L1S2Y L1S2Z] = makeLink(L1,.5,30,1);
+[L1S1X L1S1Y L1S1Z] = makeLink(L1,r1,30,1);
+[L1S2X L1S2Y L1S2Z] = makeLink(L1,r1,30,1);
 
 %  R = getRotation('X', pi);
 %  p = [ 0; 0; 0];
@@ -31,7 +34,7 @@ function [L1X L1Y L1Z L2X L2Y L2Z] = makeRCM(L1, L2, L3, r1, r2, Tz, Ty, Tx, the
 %  [L2bX L2bY L2bZ] = TransformLink(L2bX, L2bY, L2bZ, H);
  
  R = getRotation('Y', pi);
- p = [ -5; 0; 0];
+ p = [ -L2; 0; 0];
  H = [ R, p; 0 0 0 1];
  [L2S1X L2S1Y L2S1Z] = TransformLink(L2S1X, L2S1Y, L2S1Z, H);
  [L2S2X L2S2Y L2S2Z] = TransformLink(L2S2X, L2S2Y, L2S2Z, H);
@@ -41,7 +44,7 @@ function [L1X L1Y L1Z L2X L2Y L2Z] = makeRCM(L1, L2, L3, r1, r2, Tz, Ty, Tx, the
 [L2S2bX L2S2bY L2S2bZ] = makeLink(L2,r2,30, 0);
 
   R = getRotation('Y', pi);
- p = [ -5; 0; 0];
+ p = [ -L2; 0; 0];
  H = [ R, p; 0 0 0 1];
  [L2S1bX L2S1bY L2S1bZ] = TransformLink(L2S1bX, L2S1bY, L2S1bZ, H);
  [L2S2bX L2S2bY L2S2bZ] = TransformLink(L2S2bX, L2S2bY, L2S2bZ, H);
@@ -50,20 +53,20 @@ function [L1X L1Y L1Z L2X L2Y L2Z] = makeRCM(L1, L2, L3, r1, r2, Tz, Ty, Tx, the
  %% Transfroms for RCM Position
  T1 = DHTransform(Tz,0,0,-sym(pi)/2);
  T2 = DHTransform(Ty,sym(pi/2),0,sym(pi)/2);
- T2b = DHTransform(Ty-1,sym(pi/2),0,sym(pi)/2);
+ T2b = DHTransform(Ty-endLength,sym(pi/2),0,sym(pi)/2);
  T3 = DHTransform(Tx,sym(pi),0,sym(pi)/2);
  T4 = DHTransform(0,theta1,0,sym(pi)/2);
  
- p = [ 0; 0; .75];
+ p = [ 0; 0; spacing];
  T4b1 = [ [1,0,0;0,1,0;0,0,1], p; 0 0 0 1];
- p = [ 0; 0; -1];
+ p = [ 0; 0; -spacing-depth];
  T4b2 = [ [1,0,0;0,1,0;0,0,1], p; 0 0 0 1]; 
- p = [ 0; 0; -.5];
+ p = [ 0; 0; -spacing+depth];
  T4b3 = [ [1,0,0;0,1,0;0,0,1], p; 0 0 0 1];
  
  
  T5 = DHTransform(0,theta2,L1,0);
- T5b = DHTransform(0,theta2,L1+1,0);
+ T5b = DHTransform(0,theta2,L1+endLength,0);
  T6 = DHTransform(0,(sym(pi)/2)-theta2,L2,0);
  T7 = DHTransform(0,sym(pi)/2+theta2,L3,-sym(pi)/2); 
 
